536d746e1504d589b4a43a381b349b02f5ea99bc
Rob Howdle <robhowdle94@gmail.com>
2024-02-06 15:14:16 +0000

    First Commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Darlington RnB Club</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
