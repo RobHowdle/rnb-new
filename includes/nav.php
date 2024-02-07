f00e09b71b735a0a950ff381d02a67434c3d6fee
Rob Howdle <robhowdle94@gmail.com>
2024-02-07 15:55:44 +0000

    Reworked Navbar Responsive

--- includes/nav.php
+++ includes/nav.php
@@ -3,55 +3,23 @@
         <div class="img-wrapper img-wrapper--left">
             <img src="media/img/header-guitar.png">
         </div>
-        <button class="ticket ticket--left">
+        <a href="archive" class="ticket ticket--left">
             <img src="media/img/ticket.png">
             <div class="overlay-text">
                 <span class="button-text">Archive</span>
             </div>
-        </button>
+        </a>
         <a href="/" class="logo">
             <img src="media/img/logo.png">
         </a>
-        <button class="ticket ticket--right">
+        <a href="archive" class="btn ticket ticket--right">
             <img src="media/img/ticket.png">
             <div class="overlay-text">
                 <span class="button-text">Audio</span>
             </div>
-        </button>
+        </a>
         <div class="img-wrapper img-wrapper--right">
             <img src="media/img/header-guitar.png">
         </div>
     </div>
 </header>
-
-
-
-
-
-
-
-<!-- <header class="nav-header">
-    <div class="wrapper">
-        <div class="img-wrapper--left">
-            <img src="media/img/header-guitar.png">
-        </div>
-         <button class="ticket ticket--left">
-            <img src="media/img/ticket.png">
-            <div class="overlay-text">
-                <span class="button-text">Archive</span>
-            </div>
-        </button>
-        <a href="/" class="logo">
-            <img src="media/img/logo.png">
-        </a>
-        <button class="ticket ticket--right">
-            <img src="media/img/ticket.png">
-            <div class="overlay-text">
-                <span class="button-text">Audio</span>
-            </div>
-        </button>
-        <div class="img-wrapper--right">
-            <img src="media/img/header-guitar.png">
-        </div>
-    </div>
-</header> -->
--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include('header.php'); ?>
+<?php include 'header.php'; ?>
 
 <body>
-    <?php include('includes/nav.php'); ?>
-    <?php include('footer.php'); ?>
+    <?php include 'includes/nav.php'; ?>
+    <?php 
+    $current_url = $_SERVER['REQUEST_URI'];
+
+    switch($current_url) {
+      case '/':
+        include('/includes/home_content.php');
+        break;
+
+      case '/archive.php':
+        include('/includes/archive_content.php');
+        break;
+
+      case '/audio.php':
+        include('/includes/audio_content.php');
+        break;
+      default:
+        include('/includes/home_content.php');
+        break;
+    }
+    ?>
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
