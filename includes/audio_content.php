39ddddcf0379c4aa7d2395ea5f87e2cb1b4e7212
Rob Howdle <robhowdle94@gmail.com>
2024-04-18 18:51:41 +0100

    Audio Player Working Pre Vis

--- includes/audio_content.php
+++ includes/audio_content.php
@@ -6,16 +6,23 @@
                 <p class="text-center">In the mood for some music? Try our custom music player!</p>
                 <div class="audio-grid mt-2">
                     <div class="player">
-                        <div class="controls">
+                        <div class="controls autoplay">
                             <div class="buttons">
-                                <img src="media/img/stop.png">
-                                <img src="media/img/prev.png">
-                                <img src="media/img/next.png">
-                                <img src="media/img/play.png">
+                                <img src="media/img/stop.png" id="stopBtn">
+                                <img src="media/img/prev.png" id="prevBtn">
+                                <img src="media/img/next.png" id="nextBtn">
+                                <img src="media/img/play.png" id="playBtn">
                             </div>
                         </div>
+                        <audio id="audioPlayer"></audio>
                         <div class="record-player">
                             <img src="media/img/record-player.png">
+                            <div class="overlay">
+                                <img src="media/img/record.png">
+                                <div class="viswrapper">
+                                    <canvas id="visualiser"></canvas>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -73,5 +80,67 @@
             disableOnInteraction: true,
             pauseOnMouseHover: true,
         },
+    });
+
+    // Audio Player
+    const audioPlayer = document.getElementById("audioPlayer");
+    const prevButton = document.getElementById("prevBtn");
+    const playButton = document.getElementById("playBtn");
+    const stopButton = document.getElementById("stopBtn");
+    const nextButton = document.getElementById("nextBtn");
+
+    const playlist = [
+        'media/audio/All My Life.mp3',
+        'media/audio/Back for More.mp3',
+        'media/audio/Beware.mp3',
+        'media/audio/Blues With A Feeling.mp3',
+        'media/audio/Boogie Man.mp3',
+        'media/audio/Break it Down.mp3',
+        'media/audio/Bring It On Home To Me.mp3',
+        'media/audio/Divin Duck.mp3',
+        'media/audio/No Time for Jive.mp3',
+        'media/audio/Resting In The Blues.mp3',
+        'media/audio/Roxette.mp3',
+        'media/audio/Shake Your Hips.mp3',
+        'media/audio/Stop Breakin Down.mp3',
+        'media/audio/Sugar Man.mp3',
+        'media/audio/Sugar Ruh.mp3',
+        'media/audio/Travellin Light.mp3',
+        'media/audio/You Gave Me The Blues.mp3'
+    ];
+
+    let currentTrackIndex = 0;
+
+    function playTrack() {
+        audioPlayer.src = playlist[currentTrackIndex];
+        audioPlayer.play();
+        // updateTrackDisplay();
+    }
+
+    function updateTrackDisplay() {
+        trackDisplay.textContent = playlist[currentTrackIndex];
+    }
+
+    playButton.addEventListener('click', () => {
+        if (audioPlayer.paused) {
+            playTrack();
+        } else {
+            audioPlayer.pause();
+        }
+    });
+
+    nextButton.addEventListener('click', () => {
+        currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
+        playTrack();
+    });
+
+    prevButton.addEventListener('click', () => {
+        currentTrackIndex = (currentTrackIndex - 1) % playlist.length;
+        playTrack();
+    });
+
+    stopButton.addEventListener('click', () => {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
     });
 </script>
