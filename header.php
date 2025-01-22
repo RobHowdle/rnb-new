16aa03bffef7543d9949127a0eb1829d05cdff12
Rob Howdle <robhowdle94@gmail.com>
2025-01-22 15:56:21 +0000

    Updates and Favicon

--- header.php
+++ header.php
@@ -9,17 +9,24 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="RnB Club" />
+    <link rel="manifest" href="/site.webmanifest" />
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        jQuery(function($) {
-            $(".eventlist").gCalReader({
-                sortDescending: false,
+        document.addEventListener("DOMContentLoaded", function() {
+            jQuery(function($) {
+                $(".eventlist").gCalReader({
+                    sortDescending: false,
+                });
             });
         });
-    });
     </script>
     <script src="js/index.js"></script>
 </head>
